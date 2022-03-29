e1c8824c64b4c8a459f2838191f694299c06ef8b
夜光喵喵 <ShadowPower@users.noreply.github.com>
2022-03-29 13:55:00 +0800

    完成转码

--- src/infra/transcoder.rs
+++ src/infra/transcoder.rs
@@ -3,7 +3,7 @@ extern crate ffmpeg_next as ffmpeg;
 use std::path::Path;
 
 use anyhow::{Ok, Result};
-use ffmpeg::{format, frame, Packet};
+use ffmpeg::{format, frame, Packet, decoder, encoder};
 
 use super::audio_utils;
 
@@ -16,6 +16,41 @@ pub struct Transcoder {
 }
 
 impl Transcoder {
+    fn receive_and_process_encoded_packet(
+        decoder: &mut decoder::Audio,
+        encoder: &mut encoder::Audio,
+        output_time_base: ffmpeg::Rational,
+        output_ctx: &mut format::context::Output
+    ) -> Result<()> {
+        // 取得编码后的音频数据
+        let mut encoded = Packet::empty();
+        while encoder.receive_packet(&mut encoded).is_ok() {
+            encoded.set_stream(0);
+            encoded.rescale_ts(decoder.time_base(), output_time_base);
+            encoded.write_interleaved(output_ctx)?;
+        }
+        Ok(())
+    }
+
+    fn receive_and_process_decoded_frame (
+        decoder: &mut decoder::Audio,
+        encoder: &mut encoder::Audio,
+        output_time_base: ffmpeg::Rational,
+        output_ctx: &mut format::context::Output
+    ) -> Result<()> {
+        // 取得解码后的音频数据
+        let mut decoded = frame::Audio::empty();
+        while decoder.receive_frame(&mut decoded).is_ok() {
+            let timestamp = decoded.timestamp();
+            decoded.set_pts(timestamp);
+
+            // 发送给编码器
+            encoder.send_frame(&decoded)?;
+            Transcoder::receive_and_process_encoded_packet(decoder, encoder, output_time_base, output_ctx)?;
+        }
+        Ok(())
+    }
+
     pub fn transcode<P: AsRef<Path>>(&self, input: &P, output: &P) -> Result<()> {
         // 输入输出上下文
         let mut input_ctx = format::input(&input)?;
@@ -40,37 +75,31 @@ impl Transcoder {
         let (mut encoder, output_time_base) = audio_utils::create_encoder_with_output_ctx(
             codec, &mut output_ctx, channels, sample_rate, bit_rate, max_bit_rate)?;
 
-        // 开始转码
-
         // 写文件头
         output_ctx.set_metadata(input_ctx.metadata().to_owned());
         output_ctx.write_header()?;
 
+        // 开始转码
         for (stream, mut packet) in input_ctx.packets() {
             // 取出容器内的音频数据
             if stream.index() == audio_stream_index {
                 // 转换时间基
                 packet.rescale_ts(stream.time_base(), decoder.time_base());
                 decoder.send_packet(&packet)?;
-                // 取得解码后的音频数据
-                let mut decoded = frame::Audio::empty();
-                while decoder.receive_frame(&mut decoded).is_ok() {
-                    let timestamp = decoded.timestamp();
-                    decoded.set_pts(timestamp);
-
-                    // 发送给编码器
-                    encoder.send_frame(&decoded)?;
-                    // 取得编码后的音频数据
-                    let mut encoded = Packet::empty();
-                    while encoder.receive_packet(&mut encoded).is_ok() {
-                        encoded.set_stream(0);
-                        encoded.rescale_ts(decoder.time_base(), output_time_base);
-                        encoded.write_interleaved(&mut output_ctx)?;
-                    }
-                }
+                Transcoder::receive_and_process_decoded_frame(&mut decoder, &mut encoder, output_time_base, &mut output_ctx)?;
             }
         }
 
+        // 解码结束
+        decoder.send_eof()?;
+        Transcoder::receive_and_process_decoded_frame(&mut decoder, &mut encoder, output_time_base, &mut output_ctx)?;
+
+        // 编码结束
+        encoder.send_eof()?;
+        Transcoder::receive_and_process_encoded_packet(&mut decoder, &mut encoder, output_time_base, &mut output_ctx)?;
+
+        // 写文件尾
+        output_ctx.write_trailer()?;
         Ok(())
     }
 }
